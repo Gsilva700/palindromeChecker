ca77ced2d139af69ce627a923c5f56eac0998dc5
Gabriel Silva <79112647+Gsilva700@users.noreply.github.com>
2025-03-05 12:38:07 -0300

    Add files via upload

---
+++ palindromeChecker/assets/script.js
@@ -0,0 +1,18 @@
+document.getElementById('check-btn').addEventListener('click', function() {
+    const inputText = document.getElementById('text-input').value;
+    const resultElement = document.getElementById('result');
+  
+    if (!inputText) {
+      alert('Please input a value');
+      return;
+    }
+  
+    const cleanedInput = inputText.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedInput = cleanedInput.split('').reverse().join('');
+  
+    if (cleanedInput === reversedInput) {
+      resultElement.textContent = `${inputText} is a palindrome`;
+    } else {
+      resultElement.textContent = `${inputText} is not a palindrome`;
+    }
+  });
